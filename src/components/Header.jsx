4ef3c68374ad4a790da47a8d143027af2ad87a5d
kswarn <swarnakadagadkai1@gmail.com>
2021-02-03 08:01:16 +0530

    Image and header modified.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import ProfileImg from "./images/profile.jpeg";
+import ProfileImg from "./images/profile.jpeg";
 
 function Header() {
   return (
     <div>
-      {/* <img src={ProfileImg} alt="Profile" /> */}
+      <img src={ProfileImg} alt="Profile" />
       <h2>Swarna Kadagadkai</h2>
       <h4>
-        I am an Information Science graduate from JSS Academy of Technical
-        Education, Bengaluru.
+        I am an Information Science graduate from <br /> JSS Academy of
+        Technical Education, Bengaluru.
       </h4>
     </div>
   );
